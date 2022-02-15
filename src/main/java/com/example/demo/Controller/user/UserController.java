f85715e90d0d0d9e2ce9e74e50cd2f7645e693aa
Balaji. S <78396725+balaji-13@users.noreply.github.com>
2022-02-15 20:00:26 +0530

    Add files via upload

--- src/main/java/com/example/demo/Controller/user/UserController.java
+++ src/main/java/com/example/demo/Controller/user/UserController.java
@@ -1,12 +1,13 @@
 package com.example.demo.Controller.user;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,9 +21,20 @@ public class UserController {
 	@Autowired
 	UserService uservice;
 	
+	@GetMapping("/getUsers")
+	public List<UserModel> getUsers(){
+		return uservice.getUsers();		
+	}
+	
+	@GetMapping("/displayUser")
+	public UserModel displayUser(@RequestParam int UserID){
+		return uservice.displayUser(UserID);		
+	}
+	
 	@PutMapping("/editUser")
 	public UserModel editUser(@RequestBody UserModel usermodel)
 	{
+		System.out.println(usermodel);
 		return uservice.editUser(usermodel);
 	}
 	
--- src/main/java/com/example/demo/Service/UserService.java
+++ src/main/java/com/example/demo/Service/UserService.java
@@ -1,8 +1,14 @@
 package com.example.demo.Service;
 
+import java.util.List;
+
 import com.example.demo.Model.UserModel;
 
 public interface UserService {
+	
+	public UserModel displayUser(int UserID);
+	
+	public List<UserModel> getUsers();
 
 	public UserModel editUser(UserModel usermodel);
 	
