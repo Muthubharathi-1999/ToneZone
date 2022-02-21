54ab5acc17a900b32de35b4909d5ce953bdc33ba
Balaji. S <78396725+balaji-13@users.noreply.github.com>
2022-02-21 19:47:32 +0530

    Update ToneZoneApplication.java

--- Tone_Zone/src/main/java/com/example/demo/ToneZoneApplication.java
+++ Tone_Zone/src/main/java/com/example/demo/ToneZoneApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ToneZoneApplication {
 
 	public static void main(String[] args) {
-		System.setProperty("server.port", "8081");
+		//System.setProperty("server.port", "8081");
 		SpringApplication.run(ToneZoneApplication.class, args);
 		
 	}
