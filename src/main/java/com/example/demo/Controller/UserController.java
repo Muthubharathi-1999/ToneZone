2184781a09a91341ea1cb121b58127c7dc149a50
Jash Doshi <jashdoshi24@gmail.com>
2022-02-22 00:14:01 +0530

    refactor: made only one controller, deleted other

--- src/main/java/com/example/demo/Controller/UserController.java
+++ src/main/java/com/example/demo/Controller/UserController.java
@@ -9,20 +9,46 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.Model.User;
 import com.example.demo.Services.UserService;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.util.List;
 
 @RestController
 @RequestMapping("/")
 public class UserController {
 	
 	@Autowired
-	UserService userservice;
+	UserService userService;
 	
 	
 	@PostMapping(path="user/signup", consumes=MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE )
 	public String userSignup(@RequestBody User user) {
-		return userservice.userSignup(user);
-		
-		
+		return userService.userSignup(user);
+	}
+
+	@GetMapping("/getUsers")
+	public List<User> getUsers(){
+		return userService.getUsers();		
+	}
+	
+	@GetMapping("/displayUser")
+	public User displayUser(@RequestParam int UserID){
+		return userService.displayUser(UserID);		
+	}
+	
+	@PutMapping("/editUser")
+	public User editUser(@RequestBody User usermodel)
+	{
+		System.out.println(usermodel);
+		return userService.editUser(usermodel);
+	}
+	
+	@DeleteMapping("/deleteUser")
+	public void deleteUser(@RequestParam int UserID)
+	{
+		userService.deleteUser(UserID);
 	}
 	
 	
